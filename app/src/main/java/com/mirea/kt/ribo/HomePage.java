25bc5aaa8cf91631614ce84049718db5a50ac640
Фархад <toporicom@gmail.com>
2024-05-11 13:25:17 +0300

    Initial commit

---
+++ app/src/main/java/com/mirea/kt/ribo/HomePage.java
@@ -0,0 +1,18 @@
+package com.mirea.kt.ribo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/mirea/kt/ribo/MainActivity.java
@@ -0,0 +1,79 @@
+package com.mirea.kt.ribo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvTask;
+    private String login;
+    private String password;
+    private String group;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String server = "https://android-for-students.ru";
+        String serverPath = "/coursework/login.php";
+
+        //login = "Student84953";
+        //password = "iZF8k4n";
+
+        Button enterBtn = findViewById(R.id.enterBtn);
+        TextView tvError = findViewById(R.id.logError);
+        TextInputEditText etLogin = findViewById(R.id.enter_login);
+        TextInputEditText etPassword = findViewById(R.id.enter_pass);
+        enterBtn.setOnClickListener(v -> {
+            try {
+                login = etLogin.getText().toString();
+                password = etPassword.getText().toString();
+                Log.i("my_task", login);
+                Log.i("my_task", password);
+                group = "RIBO-04-22";
+                HashMap<String, String> map = new HashMap<>();
+                HTTPRunnable httpRunnable = new HTTPRunnable(server + serverPath, map);
+                Thread th = new Thread(httpRunnable);
+                map.put("lgn", login);
+                map.put("pwd", password);
+                map.put("g", group);
+                th.start();
+                try {
+                    th.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    try {
+                        JSONObject jsonObject = new JSONObject(httpRunnable.getResponseBody());
+                        Log.i("my_task", "Title: " + jsonObject.getString("title"));
+                        Log.i("my_task", "Task: " + jsonObject.getString("task"));
+                        //tvTask.setText(jsonObject.getString("task"));
+                        Log.i("my_task", "Variant: " + jsonObject.getString("variant"));
+                        tvError.setVisibility(View.GONE);
+                    } catch (JSONException e) {
+                        Intent homePageIntent = new Intent(this, HomePage.class);
+                        startActivity(homePageIntent);
+                        //throw new RuntimeException(e);
+                    }
+                }
+            }catch (RuntimeException e){
+                Log.i("my_task", "Error, invalid login or pass");
+                tvError.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
